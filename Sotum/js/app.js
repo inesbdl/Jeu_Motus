8b5d554cecb85b0cfe2be2e2fb137a1eaa1d35db
ines <bouadilin@gmail.com>
2023-12-15 13:02:56 +0100

    suite
    suite

--- Sotum/js/app.js
+++ Sotum/js/app.js
@@ -1,44 +1,101 @@
 let mot = [];
-let tab_verif_temp = [];
-let motATrouver = ["c", "o", "u", "c", "o", "u"];
+let tabTemp = [];
+let motATrouver = [];
+let resultat;
+let verif = false;
 
-let i = 0;
+fetch("https://trouve-mot.fr/api/random")
+  .then((response) => response.json())
+  .then((words) => {
+    setTimeout(() => {
+      words.forEach(word => {
+        motATrouver = word.name.split('');
+        // console.log(motATrouver);
+      })
+      Main(tabTemp);
+    }, 2000)
+  });
 
-let affichage = document.querySelector('#cadre');
-let ul = affichage.appendChild(document.createElement("ul"));
+function Main(tabTemp) {
+  console.log(motATrouver);
 
-//____________________________ D2CLARATION VARIABLES _________________________________________
+  let i = 0;
+  let indexTabTemp = 0;
 
-motATrouver.forEach(_ => {
-  let li = ul.appendChild(document.createElement("li"));
-  li.innerText = " _ ";
-});
+  let affichage = document.querySelector('#cadre');
+  let ul = affichage.appendChild(document.createElement("ul"));
 
-document.addEventListener("keydown", (event) => {
-  if (/^[A-Za-z]$/i.test(event.key) && mot.length < motATrouver.length) {
-    // if (){
-    //   return;
-    // }
-    mot.push(event.key)
-    ul.children[i].innerText = event.key;
-    i++;
-  }
-  else if (event.key === "Backspace" && mot.length > 0) {
-    i--;
-    mot.pop();
-    ul.children[i].innerText = " _ ";
-  }
-});
+  //____________________________ D2CLARATION VARIABLES _________________________________________
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    for (let index = 0; index < motATrouver.length; index++) {
-      if (mot[index] === motATrouver[index]) {
-        ul.children[index].style.backgroundColor = "red";
+
+  motATrouver.forEach(_ => {
+    let li = ul.appendChild(document.createElement("li"));
+    if (tabTemp.length == 0) {
+      li.innerText = " _ ";
+    }
+    else {
+      li.innerText = tabTemp[indexTabTemp];
+      indexTabTemp++;
+    }
+
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (/^[A-Za-z]$/i.test(event.key) && mot.length < motATrouver.length) {
+
+      mot.push(event.key)
+      ul.children[i].innerText = event.key;
+      i++;
+    }
+    else if (event.key === "Backspace" && mot.length > 0) {
+      i--;
+      mot.pop();
+      ul.children[i].innerText = " _ ";
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      for (let index = 0; index < motATrouver.length; index++) {
+        if (mot[index] === motATrouver[index]) {
+          ul.children[index].style.backgroundColor = "red";
+        }
+      }
+
+
+      mot.forEach(lettre => {
+
+        if (ul.children[mot.indexOf(lettre)].style.backgroundColor === "red") {
+          tabTemp.push(lettre);
+        }
+        else {
+          tabTemp.push(" _ ");
+        }
+        tabTemp.forEach(lettre => {
+          if (lettre === " _ ") {
+            Main(tabTemp);
+          }
+          else {
+            resultat = tabTemp.join("");
+            verif = true
+          }
+        });
+
+      });
+      // console.log(tabTemp);
+      if (verif) {
+        // console.log("sofuosougbsbs");
+        let display = document.createElement("h2");
+        display.innerText = resultat;
+        ul.append(display);
       }
     }
-  }
-});
+
+  });
+
+}
+
+
 
 
 // let verif = false;
