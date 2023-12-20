73e768e465ecdccaf9bdbbb3def8719b61dc1fc8
ines <bouadilin@gmail.com>
2023-12-20 11:24:37 +0100

    pbm résolu addEventListener
    Ajout fct au addEventListener
    Suppression btn reload du html -> js
    Style btn

--- Sotum/js/app.js
+++ Sotum/js/app.js
@@ -1,14 +1,10 @@
-document.querySelector('#reloadButton').addEventListener('click', function () {
-  // reload la page
-  location.reload();
-});
 
 function sansAccents(str) {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-};
+}
 
 // déclaration des variabless générales
-const loadingGif = document.getElementById('loadingGif');
+const loadingGif = document.getElementById("loadingGif");
 let mot = [];
 // let tabTemp = [];
 let motATrouver = [];
@@ -19,23 +15,27 @@ let nombreEssais = 6;
 // creation id
 let tabId = ["a"];
 // récupération de la div où sera affiché les infos
-let affichage = document.querySelector('#cadre');
-let affichageEnTete = document.querySelector('#enTeteJs')
-let displayEssai = affichageEnTete.appendChild(document.createElement('h2'));
-let displayNbLettres = affichageEnTete.appendChild(document.createElement('h2'));
+let affichage = document.querySelector("#cadre");
+let affichageEnTete = document.querySelector("#enTeteJs");
+let displayEssai = affichageEnTete.appendChild(document.createElement("h2"));
+let displayNbLettres = affichageEnTete.appendChild(
+  document.createElement("h2")
+);
 
-loadingGif.style.display = 'block';
+loadingGif.style.display = "block";
 
 // récupération du mot à deviner via une API
 fetch("https://trouve-mot.fr/api/random")
   .then((response) => response.json())
   .then((words) => {
     setTimeout(() => {
-      words.forEach(word => {
+      words.forEach((word) => {
         // split permet de découper une chaine de caractères dans un tableau
-        motATrouverAccent = word.name.split('');
-      })
-      motATrouver = motATrouverAccent.map((lettre) => sansAccents(lettre).toUpperCase());
+        motATrouverAccent = word.name.split("");
+      });
+      motATrouver = motATrouverAccent.map((lettre) =>
+        sansAccents(lettre).toUpperCase()
+      );
       console.log(motATrouver);
       if (motATrouver.indexOf("-") !== -1 || motATrouver.length < 4) {
         location.reload();
@@ -51,19 +51,18 @@ fetch("https://trouve-mot.fr/api/random")
       }
       // appel fonction principale
       Main(tabTemp);
-      loadingGif.style.display = 'none';
-    }, 2000)
+      loadingGif.style.display = "none";
+    }, 2000);
   });
-
 
 // définition de la fonction principale
 function Main(tabTemp) {
   console.log(`tabtemp: ${tabTemp}`);
   // récupération de la div où sera affiché le jeu
-  let affichage = document.querySelector('#cadre');
+  let affichage = document.querySelector("#cadre");
   // création d'une liste dans cette div
   let ul = affichage.appendChild(document.createElement("ul"));
-  let tabIdJoined = tabId.join('');
+  let tabIdJoined = tabId.join("");
   ul.id = tabIdJoined;
   console.log(`id ul: ${ul.id}`);
 
@@ -76,18 +75,18 @@ function Main(tabTemp) {
     if (i == 0) {
       li.innerText = motATrouver[i];
       li.style.background = "#177e89";
-    }
-    else {
+    } else {
       li.innerText = tabTemp[i];
       li.style.background = "#084c61";
     }
-
-  };
+  }
   mot = [];
   mot[0] = motATrouver[0];
   let i = 0;
   // récupération de ce que l'utilisateur rentre sur le clavier
-  document.addEventListener("keydown", (event) => {
+  document.addEventListener("keydown", CreateEventKeyDown);
+
+  function CreateEventKeyDown(event) {
     // vérifier si la touche tapée est une lettre et si la limite du mot n'est pas atteinte
     if (/^[A-Za-z]$/i.test(event.key) && mot.length < motATrouver.length) {
       let lettreEntree = event.key.toUpperCase();
@@ -96,7 +95,7 @@ function Main(tabTemp) {
       // inchangée dans l'affichage et dans le tableau du mot entré par le joueur
       if (i == 0 && lettreEntree != motATrouver[0]) {
         // récupérer la bonne liste
-        let derniereListe = document.querySelector('#' + tabIdJoined);
+        let derniereListe = document.querySelector("#" + tabIdJoined);
         // si la lettre entrée est différente de la première lettre,
         // celle-ci est directement placée à la seconde place
         derniereListe.children[i].innerText = motATrouver[0];
@@ -106,16 +105,14 @@ function Main(tabTemp) {
         mot.push(lettreEntree);
         i += 2;
         console.log("Element ul.children[i] :", ul.children[i]);
-      }
-      else if (i == 0 && lettreEntree === motATrouver[0]) {
+      } else if (i == 0 && lettreEntree === motATrouver[0]) {
         // récupérer la bonne liste
-        let derniereListe = document.querySelector('#' + tabIdJoined);
+        let derniereListe = document.querySelector("#" + tabIdJoined);
         derniereListe.children[i].innerText = motATrouver[0];
         console.log("Element ul.children[i] :", ul.children[i]);
-      }
-      else {
+      } else {
         // récupérer la bonne liste
-        let derniereListe = document.querySelector('#' + tabIdJoined);
+        let derniereListe = document.querySelector("#" + tabIdJoined);
         // remplacer " _ " par la touche tapée dans la liste
         derniereListe.children[i].innerText = lettreEntree;
         derniereListe.children[i].style.backgroundColor = "#177e89";
@@ -124,13 +121,12 @@ function Main(tabTemp) {
         i++;
         console.log("Element ul.children[i] :", ul.children[i]);
       }
-
     }
     // dans le cas où la touche tapée n'est pas une lettre, vérifier si c'est un Backspace
     else if (event.key === "Backspace" && mot.length > 1) {
       // récupérer la bonne liste
-      let derniereListe = document.querySelector('#' + tabIdJoined);
-      // pop supprime le dernier élément du tableau 
+      let derniereListe = document.querySelector("#" + tabIdJoined);
+      // pop supprime le dernier élément du tableau
       mot.pop();
       i--;
       // remplacer la lettre par " _ " dans la liste
@@ -145,7 +141,7 @@ function Main(tabTemp) {
         // vérifier si la lettre à l'index est la même des les deux tableaux
         if (mot[index] === motATrouver[index]) {
           // récupérer la bonne liste
-          let derniereListe = document.querySelector('#' + tabIdJoined);
+          let derniereListe = document.querySelector("#" + tabIdJoined);
           console.log(`derniere liste: ${derniereListe}`);
           // colore le background des lettres correctement placées en rouge
           derniereListe.children[index].style.backgroundColor = "red";
@@ -157,21 +153,20 @@ function Main(tabTemp) {
         // ajouter slice indice lettre dejà placée correctement
         else if (
           mot[index] !== motATrouver[index] &&
-          (
-            // si jamais pas de tabTemp (pour le premier essai)
-            (tabTemp.length === 0 && motATrouver.slice(1).indexOf(mot[index]) !== -1) ||
+          // si jamais pas de tabTemp (pour le premier essai)
+          ((tabTemp.length === 0 &&
+            motATrouver.slice(1).indexOf(mot[index]) !== -1) ||
             // autres cas
-            (motATrouver.indexOf(mot[index]) !== -1 && tabTemp[index] === " _ ")
-          )
+            (motATrouver.indexOf(mot[index]) !== -1 &&
+              tabTemp[index] === " _ "))
         ) {
           // récupérer la bonne liste
-          let derniereListe = document.querySelector('#' + tabIdJoined);
+          let derniereListe = document.querySelector("#" + tabIdJoined);
           // background en jaune
           derniereListe.children[index].style.backgroundColor = "#f7b735";
           // remplacer
           tabTemp.splice(index, 1, " _ ");
-        }
-        else if (motATrouver.indexOf(mot[index]) === -1) {
+        } else if (motATrouver.indexOf(mot[index]) === -1) {
           tabTemp.splice(index, 1, " _ ");
         }
       }
@@ -183,20 +178,28 @@ function Main(tabTemp) {
         affichage.append(displayResult);
       }
       // mot incomplet
-      else if ((tabTemp.indexOf(" _ ") !== -1 && essai < nombreEssais)) {
+      else if (tabTemp.indexOf(" _ ") !== -1 && essai < nombreEssais) {
         tabId.push("a");
         essai++;
         displayEssai.innerText = `Essai ${essai}/${nombreEssais}`;
         // incrémentation pour le prochain essai
+        document.removeEventListener("keydown", CreateEventKeyDown);
         Main(tabTemp);
       }
       // plus d'essais restants
       else {
         let displayResult = document.createElement("h2");
-        displayResult.innerText = `Perdu ! Le mot était ${motATrouver.join('')}`;
+        displayResult.innerText = `Perdu ! Le mot était ${motATrouver.join("")}`;
         affichage.append(displayResult);
+        // Création du bouton
+        const reloadButton = document.createElement('button');
+        reloadButton.textContent = 'REJOUER';
+        reloadButton.id = 'reloadButton';
+        reloadButton.addEventListener('click', function () {
+          location.reload();
+        });
+        affichage.append(reloadButton);
       }
     }
-  });
-
+  }
 }
