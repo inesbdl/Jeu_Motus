1bed5fb07c1a560e581f3c7d161a7770b2bf9b6f
ines <bouadilin@gmail.com>
2023-12-22 08:57:41 +0100

    remplacer marquee et ajuster les règles

--- Sotum/js/functions/afficherRegles.js
+++ Sotum/js/functions/afficherRegles.js
@@ -5,16 +5,22 @@ boutonRegles.addEventListener('click', function () {
     if (regles.style.display === 'none') {
 
         regles.style.display = 'block';
+        overlay.style.display = 'block';
+
         boutonRegles.textContent = 'Masquer les règles';
-        setTimeout(() => {
-            regles.style.opacity = '1';
-            regles.style.visibility = 'visible';
-        }, 10);
+        regles.style.opacity = '1';
+        regles.style.visibility = 'visible';
+        overlay.style.opacity = '1';
+        overlay.style.visibility = 'visible';
+
     } else {
         regles.style.opacity = '0';
+        overlay.style.opacity = '0';
         regles.style.visibility = 'hidden';
         setTimeout(() => {
             regles.style.display = 'none';
+            overlay.style.display = 'none';
+
         }, 300);
         boutonRegles.textContent = 'Afficher les règles ';
     }
